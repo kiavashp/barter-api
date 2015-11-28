719ee101740145b6a953fcbc78d707a782f6eac6
kiavashp <kiavashp@gmail.com>
2015-11-28 12:54:33 -0800

    test - seed users + more tests

--- test/001-user.js
+++ test/001-user.js
@@ -18,6 +18,16 @@ var testData = {
         username: 'Test1',
         password: 'Pass1'
     },
+    users: [
+        {
+            username: 'k',
+            password: 'p'
+        },
+        {
+            username: 's',
+            password: 'p'
+        }
+    ],
     items: [
         { name:  "paper clip" },
         { name:  "paper" },
@@ -30,23 +40,35 @@ var testData = {
 
 function createTable(options) {
     return function (done) {
-        var query = r.tableCreate(options.name)
-            .run(r.connection);
+        var query = r.tableDrop(options.name)
+            .run(r.connection)
+            .catch(function (e) {
+                console.log(e.msg);
+                return Promise.resolve();
+            })
+            .then(function () {
+                return r.tableCreate(options.name)
+                    .run(r.connection);
+            }).then(function () {
 
-        if (Array.isArray(options.indexes)) {
-            options.indexes.forEach(function (i) {
-                query = query.then(function () {
-                    return r.table(options.name).indexCreate(i).run(r.connection);
+                var promise = Promise.resolve();
+
+                if (Array.isArray(options.indexes)) {
+                    options.indexes.forEach(function (i) {
+                        promise = promise.then(function () {
+                            return r.table(options.name).indexCreate(i).run(r.connection);
+                        });
+                    });
+                }
+
+                promise = promise.then(function () {
+                    done();
+                }).catch(function (e) {
+                    console.log(e.msg);
+                    done();
                 });
-            });
-        }
 
-        query = query.then(function () {
-            done();
-        }).catch(function (e) {
-            console.log(e.msg);
-            done();
-        });
+            });
     };
 }
 
@@ -90,7 +112,7 @@ suite('setup', function () {
 
     test('create table: user', { timeout: 10e3 }, createTable({
         name: 'user',
-        indexes: ['name']
+        indexes: ['token']
     }));
 
     test('create table: item', { timeout: 10e3 }, createTable({
@@ -103,7 +125,8 @@ suite('setup', function () {
 suite('test data', function () {
 
     test('user', fillTable({
-        table: 'user'
+        table: 'user',
+        data: testData.users
     }));
 
     test('item', fillTable({
@@ -123,12 +146,83 @@ suite('user', function () {
             payload: testData.userOne
         }, function (response) {
 
-            response.statusCode.should.equal(201);
-            response.headers.should.have.property('location');
-            response.headers.location.should.be.String();
-            response.should.have.property('result').equal(null);
+            response.statusCode.should.exactly(201);
+            response.headers.should.Object();
+            response.headers.should.property('location').String();
+            response.should.property('result').equal(null);
 
             testData.userOne.url = response.headers.location;
+
+            done();
+
+        });
+
+    });
+
+    test('login', function (done) {
+
+        server.inject({
+            method: 'POST',
+            url: '/session',
+            payload: {
+                username: testData.userOne.username,
+                password: testData.userOne.password
+            }
+        }, function (response) {
+
+            response.statusCode.should.exactly(200);
+            response.headers.should.Object();
+            response.should.property('result').Object();
+            response.result.should.property('token').String();
+
+            testData.userOne.token = response.result.token;
+
+            done();
+
+        });
+
+    });
+
+    test('update', function (done) {
+
+        server.inject({
+            method: 'PUT',
+            url: testData.userOne.url,
+            headers: {
+                token: testData.userOne.token
+            },
+            payload: {
+                username: 'Test1Updated'
+            }
+        }, function (response) {
+
+            response.statusCode.should.equal(200);
+            response.headers.should.Object();
+            response.headers.should.property('location').String();
+            response.should.property('result').equal(null);
+
+            done();
+
+        });
+
+    });
+
+    test('read', function (done) {
+
+        server.inject({
+            method: 'GET',
+            url: testData.userOne.url,
+            headers: {
+                token: testData.userOne.token
+            }
+        }, function (response) {
+
+            response.statusCode.should.equal(200);
+            response.headers.should.Object();
+            response.should.property('result').Object();
+            response.result.should.property('id').String();
+            response.result.should.property('username').String().exactly('Test1Updated');
+            response.result.should.property('items').Array().lengthOf(0);
 
             done();
 
